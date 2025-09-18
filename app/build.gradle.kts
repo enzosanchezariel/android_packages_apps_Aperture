ddfdcfd08e6f6a83f31b820ab29a85ae815bdbef
Enzo Ariel Sánchez <enzo.sanchez.ariel@gmail.com>
2025-09-18 18:56:45 -0300

    Aperture: Change version to make the camera updatable by the user

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "org.lineageos.aperture"
         minSdk = 26
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     buildTypes {
